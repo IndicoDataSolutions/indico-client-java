ff6807b86d6f8b6a2658849fcd69faac5a5fa033
m c <hello@goat.rocks>
2022-04-29 11:36:56 -0400

    sonatype update

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM gradle:jdk8
 
+RUN apt-get update && apt-get -y install gnupg2
 WORKDIR /src
 COPY . /src/
 RUN chmod +x ./build.sh
--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ echo "sonatypeUsername=${SONATYPE_USERNAME}" >> gradle.properties
 echo "sonatypePassword=${SONATYPE_PASSWORD}" >> gradle.properties
 echo "${SONATYPE_GPG_KEY}" >> private.key
 echo "${SONATYPE_GPG_PASSWORD}" | gpg2 --batch --yes --passphrase-fd 0 --import private.key
-./gradlew --no-daemon publishToSonatype -PsigningKeyId="${SONATYPE_GPG_KEYID}" -PsigningPassword="${SONATYPE_GPG_PASSWORD}" -PsigningKey="${SONATYPE_GPG_KEY}"
+./gradlew --no-daemon publishToSonatype -PsigningKeyId="${SONATYPE_GPG_KEYID}" -PsigningPassword="${SONATYPE_GPG_PASSWORD}" -PsigningKey="${SONATYPE_GPG_KEY}" 
