c994bcda26a10c37c05b37b0df89eb1af5674d44
meghanhickey <meghan.hickey@indico.io>
2023-11-14 11:38:07 -0500

    remove periods from directory names

--- readme_docs/build_docs.sh
+++ readme_docs/build_docs.sh
@@ -8,11 +8,12 @@ rm package-list
 
 for filename in $(find . -type f); do
   readable_filename=$(echo $filename | sed -r 's|j\-s\-o\-n|json|g; s|\-(\w)\-|\1|g; s|\-(\w[_/])|\1|g; s|\-(\w)$|\1|g; s|_|\-|g; s|/\-|/|g')
-  dirname=$(echo $readable_filename | sed -r 's|(.+com\.indico[^/]*)/.+|\1|')
+  dirname=$(echo $readable_filename | sed -r 's|(.+com\.indico[^/]*)/.+|\1|; s|\./||; s|\.|-|g')
+  dirname="./$dirname"
   if [ $(basename $filename) = "index.md" ]; then
     newlocation="$dirname.md"
   else
-    mkdir -p dirname
+    mkdir -p $dirname
     newfilename=$(echo $readable_filename | sed -r 's|(.+com\.indico[^/]*/)(.+)|\2|; s|/|\-|g')
     newlocation="$dirname/$newfilename"
   fi
