333d67f9fd33ac3ea8f90b8ccb7fe31fd8cfd568
meghanhickey <meghan.hickey@indico.io>
2023-11-15 18:16:12 -0500

    remove links

--- readme_docs/build_docs.sh
+++ readme_docs/build_docs.sh
@@ -4,8 +4,6 @@ mv build/dokka/gfm .
 cd gfm/indico-client-java
 rm package-list
 
-# make all relative links absolute
-
 for filename in $(find . -type f); do
   if [ $(basename $filename) = "index.md" ]; then
     newName=$(dirname $filename | sed -r 's|\-(\w)\-|\1|g; s|/-|/|g; s|-(\w)_|\1-|g; s|_|-|g; s|\./||; s|\.|-|g; s|/|-|2g; s|(.+)\-(\w)$|\1\2|g')
@@ -18,4 +16,6 @@ for filename in $(find . -type f); do
 done
 
 mv ../index.md ./index.md
+# clean up links
+find . -type f -name '*.md' -print0 | xargs -0 sed -i -r "s|\[([^]]*)\]\([^)]+\.md[^\)]*\)|\1|g"
 find . -type d -empty -delete
